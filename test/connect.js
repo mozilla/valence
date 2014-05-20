b48c7bd580c48f60b981a0726e34fa8187e13b50
Dave Camp <dave.camp@gmail.com>
2014-05-20 08:33:16 -0700

    I am the only person that knows how to run this test.

--- test/connect.js
+++ test/connect.js
@@ -7,17 +7,12 @@ var Port = require("volcan/port");
 
 // This is just a temp test while I set up the thing.
 describe('grunt-webdriverjs test', function () {
-    it('checks if title contains the search query', function(done) {
-        return;
-        var port = new Port.Port(8123, "localhost");
-        console.log("testing...");
+    it('checks if we can connect to a debugger server', function(done) {
+        var port = new Port.Port(9223, "localhost");
         volcan.connect(port).then(function(root) {
-            console.log("connected...");
             return root.echo("hello");
         }).then(function(greeting) {
-            console.log("got a greeting");
             assert(greeting === "hello");
-        });//.then(done);
+        }).then(done);
     });
-
 });
