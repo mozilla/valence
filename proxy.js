42720b983e15b23ccda7e9bb05f07249c885487c
Dave Camp <dave.camp@gmail.com>
2014-05-22 16:16:45 -0700

    I am going to pay for this in the afterlife.

---
+++ proxy.js
@@ -0,0 +1,59 @@
+var WebSocket = require('ws');
+var http = require('http');
+var request = require('request');
+var url = require("url");
+
+var server = http.createServer(function (req, res) {
+  var path = url.parse(req.url).pathname
+  console.log("REQUEST: " + path);
+  if (path !== "/json") {
+    return;
+  }
+  request('http://localhost:9223/json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = body.replace(/9223/gi, "9224");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(body);
+    }
+  });
+}).listen(9222);
+
+var wss = new WebSocket.Server({
+  port: 9224
+});
+
+wss.on('connection', function(ws) {
+  console.log("web socket connection on: " + ws.upgradeReq.url);
+
+  var childName = "ws://localhost:9223" + url.parse(ws.upgradeReq.url).pathname;
+  console.log("conencting to " + childName);
+  var childSocket = new WebSocket(childName);
+
+
+  var queued = [];
+
+  ws.on('message', function(message) {
+    console.log("forwarding " + message + " client->phone")
+
+    if (queued === undefined) {
+      childSocket.send(message);
+    } else {
+      queued.push(message);
+    }
+  });
+
+  childSocket.on('open', function() {
+    for (var i = 0; i < queued.length; i++) {
+      console.log("forwarding queued" + queued[i] + " client->phone");
+      childSocket.send(queued[i]);
+    }
+    queued = undefined;
+
+    console.log("opened connection!");
+    childSocket.on('message', function(message) {
+      console.log("forwarding " + message + " phone->client");
+      ws.send(message);
+    })
+  });
+});
+
